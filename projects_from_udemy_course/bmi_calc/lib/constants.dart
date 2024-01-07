543cdff2c4b87d3448481ada5a90984f219ef3db
Boom Ittiprateep <sahatrat.itti@gmail.com>
2024-01-07 13:59:45 +0700

    Change local const to file and make it global

---
+++ projects_from_udemy_course/bmi_calc/lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const double kBottomContainerHeight = 80.0;
+const Color kBottomContainerColor = Colors.redAccent;
+const Color kReusableCardColor = Color(0xff1d1e33);
+const Color kInactiveCardColor = Color.fromARGB(255, 18, 18, 31);
+
+const TextStyle kLabelStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8d8e98),
+);
+
+const TextStyle kHeightLabel = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
