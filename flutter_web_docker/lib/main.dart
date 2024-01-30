110100d6529928e1a8579b56cff3ec6e00a1a831
Boom Ittiprateep <sahatrat.itti@gmail.com>
2024-01-30 08:23:07 +0700

    new project

---
+++ flutter_web_docker/lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Calculator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+      ),
+      home: Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String _input = '';
+  double _result = 0.0;
+
+  void _updateInput(String value) {
+    setState(() {
+      _input += value;
+    });
+  }
+
+  void _clearInput() {
+    setState(() {
+      _input = '';
+      _result = 0.0;
+    });
+  }
+
+  void _calculateResult() {
+    try {
+      setState(() {
+        _result = eval(_input);
+        _input = _result.toString();
+      });
+    } catch (e) {
+      setState(() {
+        _input = 'Error';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Web Calculator'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              color: Colors.grey[200],
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  _input,
+                  style: TextStyle(fontSize: 32.0),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('7'),
+              _buildButton('8'),
+              _buildButton('9'),
+              _buildButton('/'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('4'),
+              _buildButton('5'),
+              _buildButton('6'),
+              _buildButton('*'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('1'),
+              _buildButton('2'),
+              _buildButton('3'),
+              _buildButton('-'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('0'),
+              _buildButton('.'),
+              _buildButton('='),
+              _buildButton('+'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('C', color: Colors.red),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String text, {Color? color}) {
+    return ElevatedButton(
+      onPressed: () {
+        if (text == '=') {
+          _calculateResult();
+        } else if (text == 'C') {
+          _clearInput();
+        } else {
+          _updateInput(text);
+        }
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 24.0),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: color ?? Colors.blue,
+        padding: EdgeInsets.all(20.0),
+      ),
+    );
+  }
+
+  double eval(String expression) {
+    try {
+      Parser p = Parser();
+      Expression exp = p.parse(expression);
+      ContextModel cm = ContextModel();
+      return exp.evaluate(EvaluationType.REAL, cm);
+    } catch (e) {
+      return double.nan;
+    }
+  }
+}
